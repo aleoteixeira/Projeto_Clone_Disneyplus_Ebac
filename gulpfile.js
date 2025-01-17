adb8ece021e222fc465760f0485a304c59fc7d67
aleoteixeira <aleoteixeira@outlook.com>
2025-01-17 11:48:15 -0300

    configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,30 @@
-const gulp = require("gulp");
-const sass = require("gulp-sass")(require("sass"));
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
-async function styles() {
-  return gulp
-    .src("./src/styles/*.scss")
-    .pipe(sass({ outputStyle: "compressed" }).on('error', sass.logError))
-    .pipe(gulp.dest("./dist/css"));
+function scripts() {
+  return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'));
 }
 
-async function images() {
-  const imagemin = (await import('gulp-imagemin')).default;
-  return gulp
-    .src("./src/images/**/*")
+function styles() {
+  return gulp.src('./src/styles/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
+}
+
+function images() {
+  return gulp.src('./src/images/**/*')
     .pipe(imagemin())
-    .pipe(gulp.dest("./dist/images"));
+    .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 
 exports.watch = function () {
-  gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
+  gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+  gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
+  gulp.watch('./src/images/**/*', gulp.parallel(images));
 };
